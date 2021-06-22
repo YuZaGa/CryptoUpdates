1bc6e77c83335a378f42d9f7d48ee1a156cc1213
YuZaGa <ymangr@gmail.com>
2021-06-22 18:03:41 +0530

    Deployment Error Fix 3.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import django_heroku
 import os
 
-
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
