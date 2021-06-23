36eb97cd4e6dcb64085a37c1a1b99412e27fcb40
YuZaGa <ymangr@gmail.com>
2021-06-23 08:29:07 +0530

    Deployment Error Fix 3.7

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'crypto/static'),
 ) '''
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+
 
 django_heroku.settings(locals())
